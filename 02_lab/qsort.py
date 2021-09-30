f6ee32b39be662ab35051a5690155b9b717f320c
sltKaguya <borisbott@gmail.com>
2021-09-30 12:16:47 +0300

    ignore

--- 02_lab/qsort.py
+++ 02_lab/qsort.py
@@ -1,7 +1,18 @@
+import random
+
 def qsort(Arr):
-    pivot = A[len(Arr)]
-    for i in range(len(Arr) - 1):
-        if Arr[i] < pivot:
-            
+    if len(Arr) < 2:
+        return Arr
+    else:
+        rand_ind = random.randint(0, len(Arr) - 1)
+        pivot = Arr[rand_ind]
+        Arr.pop(rand_ind)
+        left = [i for i in Arr if i < pivot]
+        right = [i for i in Arr if i >= pivot]
+
+        Sorted_arr = qsort(left) + [pivot] + qsort(right)
+        return Sorted_arr
+
 
 A = [1477, 4, 3, 2328, 234384, 3487, 24737, 42, 38]
+print(qsort(A))
