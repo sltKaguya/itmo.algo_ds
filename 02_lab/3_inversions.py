2184b8ec0d6cc694b2df31eb8c47cab8e636d777
Kaguya <bottboris@gmail.com>
2021-10-07 16:30:47 +0300

    commit

--- 02_lab/3_inversions.py
+++ 02_lab/3_inversions.py
@@ -1,16 +1,20 @@
 f_in = open("inversions.in")
 f_out = open("inversions.out", "w")
 
-def msort_count(arr):
-    if len(arr) > 1:
+def msort_inversions(arr):
+    inversions = 0
+    if len(arr) == 1:
+        return arr, 0
+    else:
         mid = arr // 2
         left = arr[:mid]
         right = arr[mid:]
 
-        msort_count(left)
-        msort_count(right)
+        left, left_inversions = msort_inversions(left)
+        right, right_inversions = msort_inversions(right)
 
         i = j = k = 0
+        
         while i < len(left) and j < len(right):
             if left[i] < right[j]:
                 arr[k] = left[i]
@@ -20,9 +24,17 @@ def msort_count(arr):
                 arr[k] = right[j]
                 j += 1
                 k +=1
+                inversions += 1
         while i < len(left):
             arr[k] = left[i]
             i += 1
+            inversions += 1
         while j < len(right):
             arr[k] = right[j]
             j += 1
+    return inversions
+
+n = int(f_in.readline().strip())
+
+unsorted_arr = [int(i) for i in f_in.readline().split()]
+print(msort_inversions(unsorted_arr))
