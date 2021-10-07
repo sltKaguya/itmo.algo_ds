cd408480c6792c6a8463dc7f4a9eb888e4e060df
Kaguya <bottboris@gmail.com>
2021-10-07 16:58:41 +0300

    commit

--- 02_lab/3_inversions.py
+++ 02_lab/3_inversions.py
@@ -2,11 +2,10 @@ f_in = open("inversions.in")
 f_out = open("inversions.out", "w")
 
 def msort_inversions(arr):
-    inversions = 0
     if len(arr) == 1:
         return arr, 0
     else:
-        mid = arr // 2
+        mid = len(arr) // 2
         left = arr[:mid]
         right = arr[mid:]
 
@@ -14,9 +13,10 @@ def msort_inversions(arr):
         right, right_inversions = msort_inversions(right)
 
         i = j = k = 0
-        
+        inversions = left_inversions + right_inversions
+
         while i < len(left) and j < len(right):
-            if left[i] < right[j]:
+            if left[i] <= right[j]:
                 arr[k] = left[i]
                 i += 1
                 k += 1
@@ -24,17 +24,20 @@ def msort_inversions(arr):
                 arr[k] = right[j]
                 j += 1
                 k +=1
-                inversions += 1
+                inversions += (len(left) - i)
         while i < len(left):
             arr[k] = left[i]
             i += 1
-            inversions += 1
+            k += 1
         while j < len(right):
             arr[k] = right[j]
             j += 1
-    return inversions
+            k += 1
+    return arr, inversions
 
 n = int(f_in.readline().strip())
 
 unsorted_arr = [int(i) for i in f_in.readline().split()]
-print(msort_inversions(unsorted_arr))
+print(unsorted_arr)
+sorted_arr, inversions = msort_inversions(unsorted_arr)
+print(inversions, file=f_out)
