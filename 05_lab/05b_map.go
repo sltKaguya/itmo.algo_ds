c41621b1d426439dd64710f659a620f0c95af5d9
sltKaguya <bottboris@gmail.com>
2021-11-26 14:47:05 +0300

    lab 5 task 2

--- 05_lab/05b_map.go
+++ 05_lab/05b_map.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"math"
 	"os"
+	"strings"
 )
 
 type Node struct {
@@ -25,6 +27,8 @@ func (list *LinkedList) put(index int, value string) {
 		}
 		elem.next = list.first
 		list.first = elem
+	} else {
+		list.get(index).value = value
 	}
 }
 
@@ -56,8 +60,43 @@ func (list *LinkedList) delete(index int) {
 	}
 }
 
+func hash(key string, size int) int {
+	hash_sum := 5381
+	for _, elem := range key {
+		hash_sum = ((hash_sum << 5) + hash_sum) + int(elem)
+	}
+	return hash_sum
+}
+
 func main() {
-	f_in, _ := os.Open("set.in")
+	f_in, _ := os.Open("map.in")
 	scanner := bufio.NewScanner(f_in)
 	scanner.Split(bufio.ScanLines)
+	var results []string
+	size := 100000
+	hash_table := make([]LinkedList, size)
+
+	for scanner.Scan() {
+		command := strings.Fields(scanner.Text())
+		index_hashed := hash(command[1], size)
+		index := int(math.Abs(float64(index_hashed % size)))
+
+		switch command[0] {
+		case "put":
+			value := command[2]
+			hash_table[index].put(index_hashed, value)
+
+		case "delete":
+			hash_table[index].delete(index_hashed)
+		case "get":
+			if hash_table[index].get(index_hashed) != nil {
+				results = append(results, hash_table[index].get(index_hashed).value)
+			} else {
+				results = append(results, "none")
+			}
+		}
+	}
+	f_out, _ := os.Create("map.out")
+	f_out.WriteString(strings.Join(results, "\n"))
+	f_out.Close()
 }
