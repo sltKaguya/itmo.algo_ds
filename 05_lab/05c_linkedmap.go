b5ea7862cbcce24e54b8c97b51c2fa72f947127e
sltKaguya <bottboris@gmail.com>
2021-11-26 18:00:27 +0300

    lab 05 tasks 2 & 3 not finished

--- 05_lab/05c_linkedmap.go
+++ 05_lab/05c_linkedmap.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"math"
 	"os"
 	"strings"
@@ -33,19 +32,6 @@ func (list *LinkedList) put(index int, value string) {
 	}
 }
 
-func (list *LinkedList) get(index int) *Node {
-	elem := list.first
-
-	for elem != nil {
-		if elem.index == index {
-			return elem
-		} else {
-			elem = elem.next
-		}
-	}
-	return elem
-}
-
 func (list *LinkedList) delete(index int) {
 	elem := list.get(index)
 
@@ -61,6 +47,37 @@ func (list *LinkedList) delete(index int) {
 	}
 }
 
+func (list *LinkedList) get(index int) *Node {
+	elem := list.first
+
+	for elem != nil {
+		if elem.index == index {
+			return elem
+		} else {
+			elem = elem.next
+		}
+	}
+	return elem
+}
+
+func (list *LinkedList) prev(index int) *Node {
+	elem := list.get(index)
+
+	if elem != nil {
+		return elem.previous
+	}
+	return elem
+}
+
+func (list *LinkedList) next(index int) *Node {
+	elem := list.get(index)
+
+	if elem != nil {
+		return elem.next
+	}
+	return elem
+}
+
 func hash(key string, size int) int {
 	hash_sum := 5381
 	for _, elem := range key {
@@ -70,36 +87,50 @@ func hash(key string, size int) int {
 }
 
 func main() {
-	f_in, _ := os.Open("map.in")
+	f_in, _ := os.Open("linkedmap.in")
 	scanner := bufio.NewScanner(f_in)
 	scanner.Split(bufio.ScanLines)
 	var results []string
 	size := 100000
 	hash_table := make([]LinkedList, size)
-	fmt.Println(hash_table)
 
 	for scanner.Scan() {
 		command := strings.Fields(scanner.Text())
 		index_hashed := hash(command[1], size)
 		index := int(math.Abs(float64(index_hashed % size)))
+		check := hash_table[index].get(index)
 
 		switch command[0] {
 		case "put":
 			value := command[2]
-			hash_table[index].put(index_hashed, value)
+			hash_table[index].put(index, value)
 
 		case "delete":
-			hash_table[index].delete(index_hashed)
+			hash_table[index].delete(index)
 
 		case "get":
-			if hash_table[index].get(index_hashed) != nil {
-				results = append(results, hash_table[index].get(index_hashed).value)
+			if check != nil {
+				results = append(results, check.value)
+			} else {
+				results = append(results, "none")
+			}
+
+		case "prev":
+			if hash_table[index].prev(index) != nil {
+				results = append(results, check.previous.value)
+			} else {
+				results = append(results, "none")
+			}
+
+		case "next":
+			if hash_table[index].prev(index) != nil {
+				results = append(results, check.next.value)
 			} else {
 				results = append(results, "none")
 			}
 		}
 	}
-	f_out, _ := os.Create("map.out")
+	f_out, _ := os.Create("linedmap.out")
 	f_out.WriteString(strings.Join(results, "\n"))
 	f_out.Close()
 }
